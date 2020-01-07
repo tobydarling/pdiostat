6f526a288a5f316ca3f1453c4afe7e74702589a3
Toby Darling <anothercoffee@gmail.com>
2020-01-07 12:14:19 +0000

    update README; renamed function; tidy up

--- README.md
+++ README.md
@@ -10,4 +10,19 @@ group ''beegfs'', the following should return their hostnames:
 pdsh -g beegfs hostname
 ```
 
+## Usage
+```
+pdiostat.sh [-g group] [-k sort_field] [-s search_string]
+  group        : pdsh group defined in .dsh/group/"
+  sort_field   : integer 3-15, sorting iostat output, default=$field"
+  search_string: string to highlight, eg "ceph1:.*"
+```
+Eg: To monitor the ceph cluster, sorting on await, highlighting disk sda on ceph1:
+```
+pdiostat-g ceph -k 11 -s "ceph1:.*sda .*"
+```
 
+
+## License
+
+This project is licensed under the GPL v3 License - see the [LICENSE.md](LICENSE.md) file for details
--- pdiostat.sh
+++ pdiostat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get iostat output from all machines in a cluster, show the
-# top 10 stressed disks.
+# top 30 stressed disks.
 
 field=15
 usage() {
@@ -29,23 +29,12 @@ shift $((OPTIND - 1))
 pdsh -g $group hostname > /dev/null || { echo "Do you have a file ~/.dsh/group/$group?"; exit; }
 export group field
 
-f() {
-    pdsh -g $group 'iostat -mxy 1 1 | egrep -v "^dm|^md" ' | tr -s ' ' | sort -rnk$field -t' ' | head -30 | (echo -ne 'host\t'; iostat -mx | grep Device; cat -) | column -tx | sed -e "s/\($1\)/`tput smso`\1`tput rmso`/g"
+pdiostat() {
+    pdsh -g $group 'iostat -mxy 1 1 | egrep -v "^dm|^md"' |\
+      tr -s ' ' | sort -rnk$field -t' ' | head -30 |\
+      (echo -ne 'host\t'; iostat -mx | grep Device; cat -) |\
+      column -tx | sed -e "s/\($1\)/`tput smso`\1`tput rmso`/g"
 }
-### old beegfs2 only has 12 columns of iostat output :-(
-f2() {
-    pdsh -g beegfs2 'iostat -mxy 1 1' | tr -s ' ' | sort -rnk13 -t' ' | head -10 | (echo -ne 'host\t'; (ssh rat13 iostat -mx | grep Device); cat -) | column -tx | sed -e "s/\($1\)/`tput smso`\1`tput rmso`/g"
-}
 
-export -f f f2
-case $group in
-    beegfs2)
-        watch --color -n5 bash -c '"'f2 $s'"'
-    ;;
-    * )
-        watch --color -n5 bash -c '"'f $s'"'
-    ;;
-esac
-
-
-# watch -n5 "pdsh -g beegfs3 'iostat -mxy 1 1 | grep -v Device' | tr -s ' ' | sort -rnk15 -t' ' | head -10 | (echo -ne 'host\t'; iostat -mx | grep Device; cat -) | column -tx"
+export -f pdiostat
+watch --color -n5 bash -c '"'pdiostat $s'"'
